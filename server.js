f3f31d0f9be2dc4b019af9b84bcf5cb120b8ee4a
Pablo <litotechenterprise@gmail.com>
2020-06-26 19:09:40 -0400

    adjusted server.js

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
 // deploying mongoDB
-
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8082;
 const bodyParser = require('body-parser');
 require('./db/mongoose.js')
 app.use(bodyParser.json());
-// const sessions = require('express-session');
-// app.use(sessions({
-//   secret:' SECRETKEY',
-//   resave:false,
-//   saveUninitialized:false,
-// }))
+const sessions = require('express-session');
+app.use(sessions({
+  secret:' SECRETKEY',
+  resave:false,
+  saveUninitialized:false,
+}))
 
 app.get('/', (req,res) => {
     res.send('Welcome to the GreenLinks API')
 })
 
-app.listen(PORT, function() {
-  console.log("listening on Port: "+ PORT)
-})
+const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
+app.use('/events', eventRoute);
+app.use('/feed', feedRoute);
+app.use('/user', userRoute);
+app.use('/friends', friendRoute);
 
-// const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
-// app.use('/events', eventRoute);
-// app.use('/feed', feedRoute);
-// app.use('/user', userRoute);
-// app.use('/friends', friendRoute);
-
-// const http = require('http')
-// const socketio = require('socket.io')
-// ///  SOCKET.IO
-// const server = http.createServer(app)
-// server.listen(PORT, function(){
-//   console.log('Server is runing on PORT:', PORT);
-// });
-// const io = socketio(server)
-// convoRoute(app, io);
+const http = require('http')
+const socketio = require('socket.io')
+///  SOCKET.IO
+const server = http.createServer(app)
+server.listen(PORT, function(){
+  console.log('Server is runing on PORT:', PORT);
+});
+const io = socketio(server)
+convoRoute(app, io);
