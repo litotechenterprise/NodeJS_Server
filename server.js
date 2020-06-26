a5c97ba41117f5c190897008e3161dd365f70e2b
Pablo <litotechenterprise@gmail.com>
2020-06-26 18:45:33 -0400

    cancelled everything from file

--- server.js
+++ server.js
@@ -19,19 +19,23 @@ app.get('/', (req,res) => {
     res.send('Welcome to the GreenLinks API')
 })
 
-const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
-app.use('/events', eventRoute);
-app.use('/feed', feedRoute);
-app.use('/user', userRoute);
-app.use('/friends', friendRoute);
+app.listen(PORT, function() {
+  console.log("listening on Port: "+ PORT)
+})
 
-const http = require('http')
-const socketio = require('socket.io')
-///  SOCKET.IO
-const server = http.createServer(app)
-server.listen(PORT, function(){
-  console.log('Server is runing on PORT:', PORT);
-});
-const io = socketio(server)
-convoRoute(app, io);
+// const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
+// app.use('/events', eventRoute);
+// app.use('/feed', feedRoute);
+// app.use('/user', userRoute);
+// app.use('/friends', friendRoute);
+
+// const http = require('http')
+// const socketio = require('socket.io')
+// ///  SOCKET.IO
+// const server = http.createServer(app)
+// server.listen(PORT, function(){
+//   console.log('Server is runing on PORT:', PORT);
+// });
+// const io = socketio(server)
+// convoRoute(app, io);
 //require('./db/mongoose.js')
