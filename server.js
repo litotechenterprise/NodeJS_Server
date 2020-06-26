744352a69a2649f90c8dc837eb5db7a54403a861
Pablo <litotechenterprise@gmail.com>
2020-06-26 17:06:03 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 8081;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.get('/', (req,res) => {
+    res.send('Welcome to the GreenLinks API')
+  })
+
+
+app.listen(PORT, () => {
+    console.log("Listening to port " + PORT)
+})
