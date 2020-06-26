74164eb48732cb8696971432c5b11ab6e1b8fcb3
Pablo <litotechenterprise@gmail.com>
2020-06-26 19:31:38 -0400

    removing code

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const bodyParser = require('body-parser');
 //require('./db/mongoose.js')
 app.use(bodyParser.json());
 const sessions = require('express-session');
-app.use(sessions({
-  secret:' SECRETKEY',
-  resave:false,
-  saveUninitialized:false,
-}))
+// app.use(sessions({
+//   secret:' SECRETKEY',
+//   resave:false,
+//   saveUninitialized:false,
+// }))
 
 app.get('/', (req,res) => {
     res.send('Welcome to the GreenLinks API')
+})
+app.listen(PORT, function(){
+  console.log("listening on port "+PORT)
 })
 
 const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
@@ -22,12 +25,12 @@ app.use('/feed', feedRoute);
 app.use('/user', userRoute);
 app.use('/friends', friendRoute);
 
-const http = require('http')
-const socketio = require('socket.io')
+// const http = require('http')
+// const socketio = require('socket.io')
 ///  SOCKET.IO
-const server = http.createServer(app)
-server.listen(PORT, function(){
-  console.log('Server is runing on PORT:', PORT);
-});
-const io = socketio(server)
-convoRoute(app, io);
+// const server = http.createServer(app)
+// server.listen(PORT, function(){
+//   console.log('Server is runing on PORT:', PORT);
+// });
+// const io = socketio(server)
+// convoRoute(app, io);
