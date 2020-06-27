9ebcb955d43ceba3a93962c4defbc4273c711c91
Pablo <litotechenterprise@gmail.com>
2020-06-27 15:02:37 -0400

    added Http lib

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 // deploying mongoDB
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 //require('./db/mongoose.js')
 
 
 app.use(bodyParser.json());
-const sessions = require('express-session');
-app.use(sessions({
-  secret:' SECRETKEY',
-  resave:false,
-  saveUninitialized:false,
-}))
+// const sessions = require('express-session');
+// app.use(sessions({
+//   secret:' SECRETKEY',
+//   resave:false,
+//   saveUninitialized:false,
+// }))
 
 app.get('/', (req,res) => {
     res.send('Welcome to the GreenLinks API from lito')
 })
-app.listen(PORT, function(){
-  console.log("listening on port "+PORT)
-})
+// app.listen(PORT, function(){
+//   console.log("listening on port "+PORT)
+// })
 
 // const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
 // app.use('/events', eventRoute);
@@ -27,7 +27,10 @@ app.listen(PORT, function(){
 // app.use('/user', userRoute);
 // app.use('/friends', friendRoute);
 
-// const http = require('http')
+const http = require('http')
+http.createServer(app).listen(PORT, function() {
+  console.log("listening on port " + PORT)
+})
 // const socketio = require('socket.io')
 // //  SOCKET.IO
 // const server = http.createServer(app)
