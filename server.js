b6100699c089d4c0131b1a3088bb74e845824830
Pablo <litotechenterprise@gmail.com>
2020-06-27 14:04:30 -0400

    made changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8081;
 const bodyParser = require('body-parser');
-require('./db/mongoose.js')
+//require('./db/mongoose.js')
 
 
 app.use(bodyParser.json());
@@ -17,22 +17,22 @@ app.use(sessions({
 app.get('/', (req,res) => {
     res.send('Welcome to the GreenLinks API')
 })
-// app.listen(PORT, function(){
-//   console.log("listening on port "+PORT)
-// })
+app.listen(PORT, function(){
+  console.log("listening on port "+PORT)
+})
 
-const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
-app.use('/events', eventRoute);
-app.use('/feed', feedRoute);
-app.use('/user', userRoute);
-app.use('/friends', friendRoute);
+// const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
+// app.use('/events', eventRoute);
+// app.use('/feed', feedRoute);
+// app.use('/user', userRoute);
+// app.use('/friends', friendRoute);
 
-const http = require('http')
-const socketio = require('socket.io')
-//  SOCKET.IO
-const server = http.createServer(app)
-server.listen(PORT, function(){
-  console.log('Server is runing on PORT:', PORT);
-});
-const io = socketio(server)
-convoRoute(app, io);
+// const http = require('http')
+// const socketio = require('socket.io')
+// //  SOCKET.IO
+// const server = http.createServer(app)
+// server.listen(PORT, function(){
+//   console.log('Server is runing on PORT:', PORT);
+// });
+// const io = socketio(server)
+// convoRoute(app, io);
