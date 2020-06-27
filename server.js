17a90a4d0997b3bc330837a3bac41950c585bff5
Pablo <litotechenterprise@gmail.com>
2020-06-27 15:47:41 -0400

    uncommented all of the other code

--- server.js
+++ server.js
@@ -3,40 +3,37 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
-//require('./db/mongoose.js')
-const connectToDatabase = require('./db/db')
-//const { user} = require('./db/models');
+require('./db/mongoose.js')
+
 
 app.use(bodyParser.json());
-// const sessions = require('express-session');
-// app.use(sessions({
-//   secret:' SECRETKEY',
-//   resave:false,
-//   saveUninitialized:false,
-// }))
+const sessions = require('express-session');
+app.use(sessions({
+  secret:' SECRETKEY',
+  resave:false,
+  saveUninitialized:false,
+}))
 
 app.get('/', async (req,res) => {
- res.send("made it here")
+ res.send("Welcome to the Greenlinks Restful API")
 })
+
 // app.listen(PORT, function(){
 //   console.log("listening on port "+PORT)
 // })
 
-// const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
-// app.use('/events', eventRoute);
-// app.use('/feed', feedRoute);
-// app.use('/user', userRoute);
-// app.use('/friends', friendRoute);
+const {eventRoute, userRoute, friendRoute, feedRoute, convoRoute} = require('./Routes');
+app.use('/events', eventRoute);
+app.use('/feed', feedRoute);
+app.use('/user', userRoute);
+app.use('/friends', friendRoute);
 
 const http = require('http')
-http.createServer(app).listen(PORT, function() {
-  console.log("listening on port " + PORT)
-})
-// const socketio = require('socket.io')
-// //  SOCKET.IO
-// const server = http.createServer(app)
-// server.listen(PORT, function(){
-//   console.log('Server is runing on PORT:', PORT);
-// });
-// const io = socketio(server)
-// convoRoute(app, io);
+const socketio = require('socket.io')
+//  SOCKET.IO
+const server = http.createServer(app)
+server.listen(PORT, function(){
+  console.log('Server is runing on PORT:', PORT);
+});
+const io = socketio(server)
+convoRoute(app, io);
